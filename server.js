dd958e2c93105bb3fe6a582ea8993c6b862809bf
Nikolinka Cieloszczyk <cnikolinka@gmail.com>
2019-04-09 04:43:20 -0400

    some changes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var logger = require("morgan");
 var express = require("express");
 var app = express();
 
-
 app.use(logger("dev"));
 app.use(
   bodyParser.urlencoded({
@@ -14,18 +13,22 @@ app.use(
   })
 );
 
-
 app.use(express.static(process.cwd() + "/public"));
-
+//Require set up handlebars
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
 
+//connecting to MongoDB
+//mongoose.connect("mongodb://localhost/scraped_news");
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/ScrapedArticles";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -33,6 +36,9 @@ db.once("open", function() {
   console.log("Connected to Mongoose!");
 });
 
+var routes = require("./controller/controller.js");
+app.use("/", routes);
+//Create localhost port
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on PORT " + port);
